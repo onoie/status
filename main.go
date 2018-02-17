73e15ec869532383c13d611d6cf94ce209c01e75
TotemoRisky <risky.totemo@gmail.com>
2018-02-17 08:54:06 +0000

    DEBUG_20180217175406

--- main.go
+++ main.go
@@ -4,16 +4,34 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"time"
 )
 
 func main() {
-	content := []byte(time.Now().Local().String())
-	ioutil.WriteFile("ok", content, os.ModePerm)
+	//content := []byte(time.Now().Local().String())
+	//ioutil.WriteFile("ok", content, os.ModePerm)
+
 	html := []byte(`
 <html>
-<head></head>
-<body><center>Status<ul><li><a href="latest.json">latest.json</a></li></ul></center></body>
+  <head>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  </head>
+<body>
+  <div style="text-align: center;">
+      <h1>Status</h1>
+      <hr/>
+    <div style="font-size: 70%;">VERSION:<span id="version"></span></div>
+    <script type="text/javascript">
+      fetch(
+        "http://rawgit.com/onoie/status/master/VERSION",
+        { method: "get" }
+      )
+      .then(res => res.text())
+      .then(text => {
+        document.getElementById("version").innerHTML = text;
+      });
+    </script>
+  </div>
+</body>
 </html>
 	`)
 	ioutil.WriteFile("index.html", html, os.ModePerm)
